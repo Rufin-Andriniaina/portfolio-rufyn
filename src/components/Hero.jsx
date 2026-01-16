4881525fb4e1905503334c2562ae9984b03e6122
Rufin-Andriniaina <ruffin.andriniaina1@gmail.com>
2026-01-16 13:45:54 +0300

    changement de position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
             ))}
           </span>
 
-          <span className="xl:text-6xl md:text-4xl text-2xl tracking-wider px-2 md:px-0 xl:py-4 py-2 text-center overflow-hidden">
+          <span className="xl:text-6xl md:text-4xl text-xl tracking-wider xl:py-4 py-2 text-center overflow-hidden">
             I'm
             <span
               className={`inline-block xl:w-100 md:w-60 w-40 lg:ml-6 ml-2 font-extrabold transform origin-left transition-transform duration-300 ease-out ${
@@ -79,7 +79,7 @@ const Hero = () => {
           ></i>
         </button>
 
-        <div className="flex mr-auto md:gap-12 gap-2 absolute md:relative top-36 md:top-auto left-4  md:left-auto flex-col md:flex-row gap-y-5 md:gap-y-0 ">
+        <div className="flex mr-auto md:gap-12 gap-2 absolute md:relative top-28 md:top-auto left-4  md:left-auto flex-col md:flex-row gap-y-5 md:gap-y-0 ">
           {socialIcons.map((social, index) => (
             <a
               className={`${social.icon} text-red-500 dark:text-yellow-500 hover:text-gray-900 dark:hover:text-white xl:text-3xl text-2xl transition-colors duration-500 `}
@@ -91,7 +91,7 @@ const Hero = () => {
           ))}
         </div>
 
-        <div className="absolute lg:w-[500px] md:w-[400px] w-[250px] left-1/2 -translate-x-1/2 -z-10">
+        <div className="absolute lg:w-125 md:w-100 w-62.5 left-1/2 -translate-x-1/2 -z-10">
           <img
             className="w-full mx-auto transition-opacity duration-300"
             style={{ opacity: roadImageOpacity }}
@@ -103,11 +103,11 @@ const Hero = () => {
           </span>
 
           <div
-            className={`xl:h-[150px] h-[92px] px-3  mt-4 md:mt-0 xl:text-lg md:text-base text-xs font-light text-justify text-gray-950 dark:text-gray-200 tracking-wide ${
+            className={`xl:h-37.5 h-23 px-3 xl:text-lg md:text-base text-xs font-light text-justify text-gray-950 dark:text-gray-200 tracking-wide ${
               isTextVisible ? "scale-y-100" : "scale-y-0"
             } transform overflow-y-auto origin-top custom-scrollbar transition-transform duration-300`}
           >
-            <p className=" dark:first-letter:text-yellow-500 first-letter:text-red-500 first-letter:text-[30px] first-letter:ml-4 xl:py-3 py-1 px-1">
+            <p className=" dark:first-letter:text-yellow-500 first-letter:text-red-500 first-letter:text-[30px] first-letter:ml-4 px-1">
               {aboutText}
             </p>
           </div>
